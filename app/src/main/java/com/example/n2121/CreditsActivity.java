2e4e24132fe843322001439c2341c78f72c5cf93
Cyber_Mamriot <gs7073@bs.amalnet.k12.il>
2026-06-10 22:19:39 +0300

    credits activity

---
+++ app/src/main/java/com/example/n2121/CreditsActivity.java
@@ -0,0 +1,12 @@
+package com.example.n2121;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreditsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
